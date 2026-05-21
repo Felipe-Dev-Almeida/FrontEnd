488e0d2576820f655786ce82fd91669bcd72b474
FELIPE GONCALVES DE ALMEIDA <50242919804@info.local>
2026-05-21 16:47:27 -0300

    Adicionando atividade de Front_End

--- ReactJS/filmes/src/pages/cadastroFilme/cadastroFilme.jsx
+++ ReactJS/filmes/src/pages/cadastroFilme/cadastroFilme.jsx
@@ -1,13 +1,15 @@
-import "./cadastroFilme.css";
-import Header from ".../../components/header/header";
+import './CadastroFilme.css'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
 
- const CadastroFilme = () => {
-    return (
-        <>
-        <Header />
-        <h1>Página de cadastro de filmes</h1>
-         </>
-    )
+function CadastroFilme() {
+  return(
+    <>
+       <Header />
+       <h1>Cadastro - Filme</h1> 
+       <Footer />
+    </>
+  )
 }
 
 export default CadastroFilme
--- ReactJS/filmes/src/routes/routes.jsx
+++ ReactJS/filmes/src/routes/routes.jsx
@@ -1,28 +1,21 @@
-// instalart o pacote react-rouber-dom
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
-import Login from "../pages/login/login";
-import CadastroGenero from "../pages/cadastroGenero/cadastroGenero";
-import CadastroFilme from "../pages/cadastroFilme/cadastroFilme";
+//instalar pacote das react-router-dom
 
-const Rotas = () => {
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "../pages/login/login"
+import CadastroFilme from "../pages/cadastroFilme/cadastroFilme"
+import CadastroGenero from "../pages/cadastroGenero/cadastroGenero"
 
-     return (
-    
-    <BrowserRouter>
-    <nav>   
-      <Link to="/">Login</Link> {" "}
-      <Link to="/filme">Filmes</Link> {" "}
-      <Link to="/genero">Gênero</Link> {" "}
-    </nav>
-    <Routes>
-      <Route element = {<Login/>} path="/"/>
-      <Route element = {<CadastroGenero/>} path="/cadastro-genero"/>
-      <Route element = {<CadastroFilme/>} path="/cadastro-filme"/>
-    </Routes>
-    </BrowserRouter>
-    
-  )
 
- }
+ const Rotas = () => {
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route element={<Login />} path="/"/>
+            <Route element={<CadastroFilme />} path="/filmes"/>
+            <Route element={<CadastroGenero/>} path="/generos"/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
 
- export default Rotas
+export default Rotas
